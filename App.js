357f7881bf64ef5f2c2bbea87fbe8526a0156bc1
NashTheCoder <naijeria@gmail.com>
2020-11-03 23:19:24 +0300

    added welcome button with alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
 
 //SafeAreaView render content within the safe area boundaries of a device //
 //Image component allows you to render images on the app //
@@ -8,8 +8,12 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Welcome</Text>
-      <Image source={require('./assets/lets_talk_logo.png')} />
+      <Button 
+        title='Welcome' 
+        onPress={() => alert('Create account')} />
+        <Image source={require('./assets/lets_talk_logo.png')} />
+      <Button 
+        title='Curious?'/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
