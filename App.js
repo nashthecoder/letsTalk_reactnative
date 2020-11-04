e9fc58314b92e07866743b5db334a3937c5c2e7d
NashTheCoder <naijeria@gmail.com>
2020-11-04 13:51:20 +0300

    changed UI  of welcome page

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, Alert, Button } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  SafeAreaView, 
+  Alert, 
+  Button, 
+  TextInput } 
+  from 'react-native';
 // import { useDimensions } from '@react-native-community/hooks';
 //SafeAreaView render content within the safe area boundaries of a device in iOS , ise Platform for Android 
 // and add padding via the StatusBar.height in the StyleSheet.create
@@ -10,13 +19,19 @@ import { StyleSheet, Text, View, Image, SafeAreaView, Alert, Button } from 'reac
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/lets_talk_logo.png')} />
+      <Text style={styles.login}> Login </Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='Username'
+         />
+      <TextInput 
+        style={styles.input}
+        placeholder='Password'
+         />
+      
       <Button 
-        title='Welcome' 
-        onPress={() => 
-        Alert.alert('Click here to create an account')} />
-        <Image source={require('./assets/lets_talk_logo.png')} />
-      <Button
-        title='Curious?'
+        title ='Browsing?'
         onPress={() =>
         Alert.alert('Hey', 'Want to know more?', [
             {text: 'Yes'},
@@ -32,8 +47,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8F3F1',
     justifyContent: 'center',
     alignItems: 'center',
   },
+ login: {
+   color: 'black',
+   fontWeight: '600',
+   fontSize: 20,
+ }
 });
